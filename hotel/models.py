6e65fdb948d49d893c8410b18b7681608844c655
sagrarioducuara <sagrariocacaris2004@gmail.com>
2023-10-23 16:57:58 -0500

    usuario

--- hotel/models.py
+++ hotel/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class usuarios(models.Model):
-    Nr_doc=models.CharField(max_length=38, null=True)
+    Nr_doc=models.IntegerField(primary_key=True)
     Nombre=models.CharField(max_length=38, null=True)
     Contraseña=models.CharField(max_length=38, null=True)
     Id_rol=models.CharField(max_length=38, null=True)    
